0e9ed220ba7d4fb0fece5add92c8d2a5f5e1ba52
canergin <c.ergin@campus.tu-berlin.de>
2021-04-23 00:33:22 +0300

    Test for Blatt01

---
+++ Blatt01/src/Interneuron.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+/**
+ * The class Neuron implents a interneuron for the class Network.
+ * 
+ * @author Vera Röhr
+ * @version 1.0
+ * @since 2019-01-11
+ */
+public class Interneuron extends Neuron {
+	/**
+	 * {@inheritDoc}
+	 */
+	public Interneuron(int index) {
+		super(index);
+	}
+
+	/**
+	 * Divides incoming signal into equal parts for all the outgoing synapses
+	 * Transmits the parts via Synapse.transmit(Double[]);
+	 * 
+	 * @param signal 3 dimensional signal from another neuron
+	 * @return 3 dimensional neural signal, which got transmitted to the synapses (for testing.)
+	 */
+	@Override
+	public double[] integrateSignal(double[] signal) {
+		// TODO
+		return signal;
+	}
+}
+
+
