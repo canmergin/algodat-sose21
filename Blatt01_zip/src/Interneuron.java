57c2c626617f2ec05681e3d3eb923c212d04baf1
Service Account AlgoDat <robotics-tb-algodat@win.tu-berlin.de>
2021-06-07 16:43:46 +0200

    Testergebnisse Blatt01_zip

---
+++ Blatt01_zip/src/Interneuron.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+/**
+ * The class Neuron implents a interneuron for the class Network.
+ * 
+ * @author Vera Röhr
+ * @version 1.0
+ * @since 2019-01-11
+ */
+public class Interneuron extends Neuron {
+	/**
+	 * {@inheritDoc}
+	 */
+	public Interneuron(int index) {
+		super(index);
+		this.outgoingsynapses = new ArrayList<Synapse>();
+
+	}
+
+	/**
+	 * Divides incoming signal into equal parts for all the outgoing synapses
+	 * Transmits the parts via Synapse.transmit(Double[]);
+	 * 
+	 * @param signal 3 dimensional signal from another neuron
+	 * @return 3 dimensional neural signal, which got transmitted to the synapses (for testing.)
+	 */
+	@Override
+	public double[] integrateSignal(double[] signal) {
+		int signalLength = signal.length;
+		for (int c = 0; c < signalLength; c++) {
+			signal[c] = signal[c] / this.outgoingsynapses.size();
+		}
+		for (int i = 0; i < this.outgoingsynapses.size(); i++) {
+			this.outgoingsynapses.get(i).transmit(signal);
+		}
+		return signal;
+	}
+}
+
